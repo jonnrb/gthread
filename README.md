53855c69dbdaafff7815215bf88f61b2670f22b0
Jon Betti <jonbetti@gmail.com>
2017-10-14 21:32:48 -0400

    Benchmark `gthread_switch_to()`
    Runs about 10x faster than `swapcontext()`! We should also benchmark
    `setitimer()` and `getitimer()` and if they have less overhead than
    `gthread_tls_use()`, disabling TLS will boost performance significatly
    (that's the slowest thing in the context switch I know of at this
    point).

--- README.md
+++ README.md
@@ -7,3 +7,16 @@ For the Rutgers undergraduate Operating Systems class, we were assigned the
 task of building a user-space threading library that implements the POSIX
 threads API (pthread). Our solution, gthread accomplishes that task using
 abstractions on the base platform and architecture.
+
+## Context Switch
+
+`ucontext.h` is quite deprecated (gone from POSIX '08) and is very _very_ slow.
+We implemented our own context switch. It piggybacks off of the function
+calling convention on x86-64 and is *10x* faster than the `swapcontext()`
+context switch in `ucontext.h`.
+
+To run our microbenchmark:
+```bash
+make bin-test/arch/switch_to_test
+bin-test/arch/switch_to_test
+```
--- arch/switch_to_test.c
+++ arch/switch_to_test.c
@@ -5,27 +5,94 @@
  * info: tests switching between and spawning new contexts
  */
 
-#include <stdio.h>
+#include "arch/switch_to.h"
 
+#include <assert.h>
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <ucontext.h>
+
+#include "platform/clock.h"
 #include "platform/memory.h"
-#include "switch_to.h"
+
+#define k_num_context_switches ((uint64_t)10 * 1000 * 1000)
 
 const char* test_str = "hello world";
 
 static int good = 0;
+static int also_good = 0;
+
+static gthread_saved_ctx_t main_ctx;
+static gthread_saved_ctx_t test_func_ctx;
+
+static ucontext_t main_uctx;
+static ucontext_t utest_func_uctx;
 
 void test_func(void* arg) {
   char* c_arg = (char*)arg;
   printf("%s\n", c_arg);
   good = 1;
+
+  for (uint64_t i = 0; i < k_num_context_switches; ++i) {
+    __asm__ __volatile__("" ::: "memory");
+    gthread_switch_to(&test_func_ctx, &main_ctx);
+  }
+}
+
+void utest_func() {
+  also_good = 1;
+
+  for (uint64_t i = 0; i < k_num_context_switches; ++i) {
+    __asm__ __volatile__("" ::: "memory");
+    swapcontext(&utest_func_uctx, &main_uctx);
+  }
 }
 
 int main() {
   void* stack;
-  gthread_allocate_stack(NULL, &stack, NULL);
+  size_t stack_size;
+  gthread_allocate_stack(NULL, &stack, &stack_size);
 
-  gthread_saved_ctx_t self;
-  gthread_switch_to_and_spawn(&self, &self, stack, test_func, (void*)test_str);
+  gthread_switch_to_and_spawn(&main_ctx, &main_ctx, stack, test_func,
+                              (void*)test_str);
+
+  if (!good) {
+    printf("bad\n");
+    return -1;
+  }
+
+  uint64_t start = gthread_clock_process();
+  for (uint64_t i = 0; i < k_num_context_switches; ++i) {
+    __asm__ __volatile__("" ::: "memory");
+    gthread_switch_to(&main_ctx, &test_func_ctx);
+  }
+  uint64_t end = gthread_clock_process();
+
+  printf("gthread_switch_to() microbenchmark:\n");
+  printf("  %" PRIu64 " context switches in %.3f microseconds\n",
+         k_num_context_switches * 2, (double)(end - start) / 1000);
+  printf("  %.0f context switches / microsecond\n",
+         k_num_context_switches * 2 / ((double)(end - start) / 1000));
+
+  assert(!getcontext(&utest_func_uctx));
+  utest_func_uctx.uc_stack.ss_sp = &((char*)stack)[-stack_size];
+  utest_func_uctx.uc_stack.ss_size = stack_size;
+  utest_func_uctx.uc_link = &main_uctx;
+  makecontext(&utest_func_uctx, utest_func, 0);
+
+  start = gthread_clock_process();
+  for (uint64_t i = 0; i < k_num_context_switches; ++i) {
+    __asm__ __volatile__("" ::: "memory");
+    swapcontext(&main_uctx, &utest_func_uctx);
+  }
+  end = gthread_clock_process();
+
+  printf("swapcontext() microbenchmark:\n");
+  printf("  %" PRIu64 " context switches in %.3f microseconds\n",
+         k_num_context_switches * 2, (double)(end - start) / 1000);
+  printf("  %.0f context switches / microsecond\n",
+         k_num_context_switches * 2 / ((double)(end - start) / 1000));
 
   return !good;
 }
