ff298e131fda4ccb4e1da42c6fef4c8230fab674
JonNRb <jonbetti@gmail.com>
2017-11-10 12:10:23 -0500

    Stress tests for task module

---
+++ sched/task_test.cc
@@ -0,0 +1,50 @@
+#include "sched/task.h"
+#include "gtest/gtest.h"
+#include "gthread.h"
+
+constexpr auto k_num_stress_tasks = 10000;
+constexpr auto k_num_switches = 3;
+
+using namespace gthread;
+
+void* thread(void* arg) {
+  task* root = (task*)arg;
+  for (int i = 0; i < k_num_switches; ++i) {
+    EXPECT_EQ(root->switch_to(), 0);
+  }
+  return nullptr;
+}
+
+TEST(gthread_task, lots_of_switch_to) {
+  task* tasks[k_num_stress_tasks] = {nullptr};
+  task* root = task::current();
+  for (int i = 0; i < k_num_stress_tasks; ++i) {
+    tasks[i] = task::create(k_default_attr);
+    tasks[i]->entry = thread;
+    tasks[i]->arg = (void*)root;
+    ASSERT_EQ(tasks[i]->start(), 0);
+  }
+
+  for (int i = 0; i < k_num_switches; ++i) {
+    for (int j = 0; j < k_num_stress_tasks; ++j) {
+      ASSERT_EQ(tasks[j]->switch_to(), 0);
+    }
+  }
+}
+
+TEST(gthread_task, lots_of_switch_to_no_tls) {
+  task* tasks[k_num_stress_tasks] = {nullptr};
+  task* root = task::current();
+  for (int i = 0; i < k_num_stress_tasks; ++i) {
+    tasks[i] = task::create(k_light_attr);
+    tasks[i]->entry = thread;
+    tasks[i]->arg = (void*)root;
+    ASSERT_EQ(tasks[i]->start(), 0);
+  }
+
+  for (int i = 0; i < k_num_switches; ++i) {
+    for (int j = 0; j < k_num_stress_tasks; ++j) {
+      ASSERT_EQ(tasks[j]->switch_to(), 0);
+    }
+  }
+}
