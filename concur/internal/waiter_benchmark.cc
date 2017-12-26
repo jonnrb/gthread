865b34cdfe7ffa400bc729e6905a45e39815d82e
Jon Betti <jonbetti@gmail.com>
2017-12-25 19:53:25 -0500

    Add a small `waiter_benchmark`

---
+++ concur/internal/waiter_benchmark.cc
@@ -0,0 +1,33 @@
+#include "concur/internal/waiter.h"
+
+#include "benchmark/benchmark.h"
+#include "gthread.h"
+
+static void benchmark_waiter(benchmark::State& state) {
+  gthread::internal::waiter w;
+  auto done_flag = false;
+
+  gthread::g bg([&w, &done_flag]() {
+    while (!w.park()) {
+    }
+    while (!done_flag) {
+      while (!w.swap()) {
+      }
+    }
+  });
+
+  for (auto _ : state) {
+    while (!w.swap()) {
+    }
+  }
+
+  done_flag = true;
+  while (!w.unpark()) {
+  }
+
+  bg.join();
+}
+
+BENCHMARK(benchmark_waiter);
+
+BENCHMARK_MAIN()
